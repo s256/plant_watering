133d5a03b22f62d80c7cc6c0e5697eca7d40677b
Sebastian Schneider <sebastian.schneider@boxine.de>
2022-03-12 18:24:05 +0100

    use ota code from lib

--- plant_watering.ino
+++ plant_watering.ino
@@ -36,6 +36,7 @@ int water_level = 0;
 using namespace std;
 #include <string>
 #include <sstream>
+#include "src/ota.h"
 
 // ##############################
 // Init Smart Home Stuff
@@ -58,21 +59,8 @@ struct Measurement // Data structure
 RTC_DATA_ATTR QList<Measurement> queue; // Queue
 HTTPClient http;                        // HTTPClient user for transfering data to InfluxDB
 
-// OTA Stuff
-
-long contentLength = 0;
-long long firmware = 0;
-bool isValidContentType = false;
-bool isNewFirmware = false;
-
-String host = OTA_SERVER; // Host => OTA Server Path
-int port = 80;            // Non https. For HTTPS 443. As of today, HTTPS doesn't work.
-
-// Utility to extract header value from headers
-String getHeaderValue(String header, String headerName)
-{
-    return header.substring(strlen(headerName.c_str()));
-}
+// OTA ota;
+OTA ota(OTA_SERVER, 80, FW_VERSION);
 
 void setup()
 {
@@ -108,7 +96,7 @@ void setup()
     Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 
     // Execute OTA Update
-    execOTA();
+    ota.execOTA();
     Serial.println("OTA done...");
     WiFi.mode(WIFI_OFF);
 }
@@ -232,204 +220,6 @@ void print_wakeup_reason()
     default:
         Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
         break;
-    }
-}
-
-// ##############################
-// OTA Logic
-// ##############################
-
-void execOTA()
-{
-    Serial.println("Connecting to: " + String(host));
-    wifiMacString.replace(":", "");
-    String bin = "/ota/esp32_" + wifiMacString + ".ino.bin"; // bin file name with a slash in front.
-    // Connect to S3
-    if (espClient.connect(host.c_str(), port))
-    {
-        // Connection Succeed.
-        // Fecthing the bin
-        Serial.println("Fetching Bin: " + String(bin));
-
-        // Get the contents of the bin file
-        espClient.print(String("GET ") + bin + " HTTP/1.1\r\n" +
-                        "Host: " + host + "\r\n" +
-                        "Cache-Control: no-cache\r\n" +
-                        "Connection: close\r\n\r\n");
-
-        // Check what is being sent
-        //    Serial.print(String("GET ") + bin + " HTTP/1.1\r\n" +
-        //                 "Host: " + host + "\r\n" +
-        //                 "Cache-Control: no-cache\r\n" +
-        //                 "Connection: close\r\n\r\n");
-
-        unsigned long timeout = millis();
-        while (espClient.available() == 0)
-        {
-            if (millis() - timeout > 5000)
-            {
-                Serial.println("Client Timeout !");
-                espClient.stop();
-                return;
-            }
-        }
-        // Once the response is available,
-        // check stuff
-
-        /*
-           Response Structure
-            HTTP/1.1 200 OK
-            x-amz-id-2: NVKxnU1aIQMmpGKhSwpCBh8y2JPbak18QLIfE+OiUDOos+7UftZKjtCFqrwsGOZRN5Zee0jpTd0=
-            x-amz-request-id: 2D56B47560B764EC
-            Date: Wed, 14 Jun 2017 03:33:59 GMT
-            Last-Modified: Fri, 02 Jun 2017 14:50:11 GMT
-            ETag: "d2afebbaaebc38cd669ce36727152af9"
-            Accept-Ranges: bytes
-            Content-Type: application/octet-stream
-            Content-Length: 357280
-            Server: AmazonS3
-
-            {{BIN FILE CONTENTS}}
-
-        */
-        while (espClient.available())
-        {
-            // read line till /n
-            String line = espClient.readStringUntil('\n');
-            // remove space, to check if the line is end of headers
-            line.trim();
-
-            // if the the line is empty,
-            // this is end of headers
-            // break the while and feed the
-            // remaining `client` to the
-            // Update.writeStream();
-            if (!line.length())
-            {
-                // headers ended
-                break; // and get the OTA started
-            }
-
-            // Check if the HTTP Response is 200
-            // else break and Exit Update
-            if (line.startsWith("HTTP/1.1"))
-            {
-                if (line.indexOf("200") < 0)
-                {
-                    Serial.println("Got a non 200 status code from server. Exiting OTA Update.");
-                    break;
-                }
-            }
-
-            // extract headers here
-            // Start with content length
-            if (line.startsWith("Content-Length: "))
-            {
-                contentLength = atol((getHeaderValue(line, "Content-Length: ")).c_str());
-                Serial.println("Got " + String(contentLength) + " bytes from server");
-            }
-
-            // Next, the content type
-            if (line.startsWith("Content-Type: "))
-            {
-                String contentType = getHeaderValue(line, "Content-Type: ");
-                Serial.println("Got " + contentType + " payload.");
-                if (contentType == "application/octet-stream")
-                {
-                    isValidContentType = true;
-                }
-            }
-
-            // Next, the firmware version
-            if (line.startsWith("X-Firmware: "))
-            {
-                firmware = atoll((getHeaderValue(line, "X-Firmware: ")).c_str());
-                Serial.print("Got ");
-                Serial.print(firmware);
-                Serial.print(" version. Currently running: ");
-                Serial.print(FW_VERSION);
-                Serial.println();
-                if (firmware > FW_VERSION)
-                {
-                    Serial.println("Firmware is new. Starting Update...");
-                    isNewFirmware = true;
-                }
-                else
-                {
-                    Serial.println("Firmware is not new, skipping Update.");
-                }
-            }
-        }
-    }
-    else
-    {
-        // Connect to S3 failed
-        // May be try?
-        // Probably a choppy network?
-        Serial.println("Connection to " + String(host) + " failed. Please check your setup");
-        // retry??
-        // execOTA();
-    }
-
-    // Check what is the contentLength and if content type is `application/octet-stream`
-    Serial.println("contentLength : " + String(contentLength) + ", isValidContentType : " + String(isValidContentType) + ", isNewFirmware : " + String(isNewFirmware));
-
-    // check contentLength and content type
-    if (contentLength && isValidContentType && isNewFirmware)
-    {
-        // Check if there is enough to OTA Update
-        bool canBegin = Update.begin(contentLength);
-
-        // If yes, begin
-        if (canBegin)
-        {
-            Serial.println("Begin OTA. This may take 2 - 5 mins to complete. Things might be quite for a while.. Patience!");
-            // No activity would appear on the Serial monitor
-            // So be patient. This may take 2 - 5mins to complete
-            size_t written = Update.writeStream(espClient);
-
-            if (written == contentLength)
-            {
-                Serial.println("Written : " + String(written) + " successfully");
-            }
-            else
-            {
-                Serial.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?");
-                // retry??
-                // execOTA();
-            }
-
-            if (Update.end())
-            {
-                Serial.println("OTA done!");
-                if (Update.isFinished())
-                {
-                    Serial.println("Update successfully completed. Rebooting.");
-                    ESP.restart();
-                }
-                else
-                {
-                    Serial.println("Update not finished? Something went wrong!");
-                }
-            }
-            else
-            {
-                Serial.println("Error Occurred. Error #: " + String(Update.getError()));
-            }
-        }
-        else
-        {
-            // not enough space to begin OTA
-            // Understand the partitions and
-            // space availability
-            Serial.println("Not enough space to begin OTA");
-            espClient.flush();
-        }
-    }
-    else
-    {
-        Serial.println("There was no content in the response or Firmware is up to date");
-        espClient.flush();
     }
 }
 
